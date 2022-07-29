3574bc258deb716c1c88f029a7d980511d4277ba
Mardhem Maciel <mardhem.abreu@gmail.com>
2022-07-29 14:54:42 +0200

    folder ex005 created

---
+++ ex005/script01.js
@@ -0,0 +1,6 @@
+var speed = 79
+console.log(`Your car speed is ${speed}km/h.`) 
+if(speed > 60) {
+    console.log(`You have exceeded the maximum speed limit. You are fined.`)
+}
+console.log('Always drive with your seat belt.')
---
+++ ex005/script02.js
@@ -0,0 +1,11 @@
+var country = 'USA'
+
+console.log(`You were born in ${country}.`)
+
+if (country == 'Brazil') {
+    console.log('You are brazilian.')
+}
+
+else {
+    console.log('You are foreigner.')
+}
