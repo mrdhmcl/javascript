1909a808261bfa923678aee52ebd1a1ed1f5b97f
Mardhem Maciel <mardhem.abreu@gmail.com>
2022-09-09 15:01:43 +0200

    new folder practice created

--- ex011/function 03.js
+++ ex011/function 03.js
@@ -1,5 +1,11 @@
-let jay = function double(x){
-    return x * 2
-}
 
-console.log(jay(5))
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "My " + myNoun + " is really " + myAdjective + " and, yesterday, he escaped. And oh, boy, he " + myVerb + " very " + myAdverb + "."; // Change this line
+// Only change code above this line
+console.log(wordBlanks)
---
+++ practice/module 4/ambient.js
@@ -0,0 +1,22 @@
+let student = {
+    name: "Mardhem",
+    age: 28,
+    weight: 62.5
+}
+
+
+let students = [
+  student
+]
+
+
+
+let newStudent = {
+  name: "Ravena",
+  age: 29,
+  weight: 50
+}
+
+students.push(newStudent)
+
+console.log(students[1])
